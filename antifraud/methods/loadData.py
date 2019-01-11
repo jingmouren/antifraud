f2386d257bcf6fe0bdcb0ab65c0989a0dfa07b8b
rangma123 <46476380+rangma123@users.noreply.github.com>
2019-01-11 09:14:24 +0800

    Creat loadData.py
    Load trade data, sample train data and test data, all models use the same train and test data

---
+++ antifraud/methods/loadData.py
@@ -0,0 +1,40 @@
+def loadData(data_ratio = 0.02, test_ratio = 0.3):
+    '''
+    load trade data, including fraud trades and no_fraud trades, due to the unbalance of fraud trades and no_fraud
+    trades, we use all fraud trades and choose some no_fraud trades to train our model, when we calculate the accuracy
+    of our model, we must consider the data ratio
+    Args:
+        data_ratio: the ratio to choose no_fraud trades
+        test_ratio: the ratio for test data using in our models
+    Returns:
+        X_train: features used in model training
+        y_train: labels used in model training
+        X_test: features used in model testing
+        y_test: labels used in model testing
+    '''
+    feature = []
+    label = []
+    file_read = open('data/fraud_trade.txt')  # read fraud trades
+    for line in file_read.readlines():
+        data = line.strip().split()
+        label.append(int(data[0]))  # the first column is the label, data type: int
+        feature0 = [float(item) for item in data[1:]]  # from the second column to the end are the features, data type: float
+        feature.append(feature0)
+    X_train1, X_test1, y_train1, y_test1 = train_test_split(feature, label, test_size = test_ratio, random_state = 0)
+    feature = []
+    label = []
+    sample = int(1.0/data_ratio)  # sample number, label the trade to choose or not
+    count = 0  # count number
+    file_read = open('data/no_fraud_trade.txt')  # read no_fraud trades
+    for line in file_read.readlines():
+        if count % sample == 0:
+            data = line.strip().split()
+            label.append(int(data[0]))  # the first column is the label, data type: int
+            feature0 = [float(item) for item in data[1:]]  # from the second column to the end are the features, data type: float
+            feature.append(feature0)
+    X_train, X_test, y_train, y_test = train_test_split(feature, label, test_size=test_ratio, random_state=0)
+    X_train = X_train + X_train1  # connect the features and labels together
+    y_train = y_train + y_train1
+    X_test = X_test + X_test1
+    y_test = y_test + y_test1
+    return X_train, X_test, y_train, y_test
